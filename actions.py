b9a3753a8468183768527747f8b4d19318b28f7b
Yair <ballinasyair@gmail.com>
2025-02-27 12:06:31 -0600

    fixed a typing error

--- actions.py
+++ actions.py
@@ -5,7 +5,7 @@ class EscapeAction(Action):   # hitting the esc key will exit the game.
     pass
 
 class MovementAction(Action):   # used to describe our player moving around
-    def __innit__(self, dx: int, dy: int):
+    def __init__(self, dx: int, dy: int):
         super().__init__()
 
         # used to describe which direction the player is moving in
