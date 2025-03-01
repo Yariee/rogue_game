94ebb0f1fdaab6d94844b0f89830df4fc2598b81
Yair <ballinasyair@gmail.com>
2025-03-01 00:10:18 -0600

    passing Engine and Entity actions to Action subclasses.

--- actions.py
+++ actions.py
@@ -1,8 +1,17 @@
-class Action:
-    pass
+from __future__ import annotations
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+class Action:
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """Perform action with the objects needed to determine the scope"""
+        raise NotImplementedError()
 class EscapeAction(Action):   # hitting the esc key will exit the game.
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 class MovementAction(Action):   # used to describe our player moving around
     def __init__(self, dx: int, dy: int):
@@ -11,3 +20,14 @@ class MovementAction(Action):   # used to describe our player moving around
         # used to describe which direction the player is moving in
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination out of bounds
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by tile.
+
+        entity.move(self.dx, self.dy)
